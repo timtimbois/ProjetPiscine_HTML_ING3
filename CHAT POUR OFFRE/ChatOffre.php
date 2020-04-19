9408098a9530a17f25bbbc2e5725dcfccc402c5e
TDany <47555603+TDany@users.noreply.github.com>
2020-04-20 00:28:07 +0200

    Add files via upload
    commit du chat entre le vendeur et l'acheteur pour l'achat en meilleure offre

---
+++ CHAT POUR OFFRE/ChatOffre.php
@@ -0,0 +1,450 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+	<title>EbayECE : Chat Offre</title>
+
+
+	<!-- Bootstrap CSS CDN -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+	<!-- MAKE A LINK WITH THE CSS TO CUSTOME IT -->
+	<link href="index1.css" rel="stylesheet">
+
+	<!-- SCROLLBAR CUSTOM CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+	<!-- Font Awesome JS = AUSSI POUR LES PETITS TRAIT A COTE DE SIDEBAR-->
+	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+	<!-- LIEN CSS POUR AVOIR LES ICONES TELECHARGÉES DEPUIS ICOMOON -->
+	<link href="style.css" rel="stylesheet">
+
+	<!-- POUR DU JQUERY -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"> </script>
+
+	<!-- LIEN CSS DU CHAT POUR LES OFFRES -->
+	<link href="chat.css" rel="stylesheet">
+
+	<!-- DESACTIVER LA TOUCHE ENTRÉE DANS TOUTE CETTE PAGE -->
+	<Script> 
+		function desactiveEnter(){ 
+			if (event.keyCode == 13) { 
+				event.keyCode = 0; 
+				window.event.returnValue = false; 
+			} 
+		} 
+	</Script> 
+
+	
+
+</head>
+
+<body onKeyDown='desactiveEnter()'>
+
+	<!-- SIDEBAR -->
+	<div class="wrapper">
+
+		<!-- Sidebar -->
+		<nav id="sidebar">
+			<div class="sidebar-header">
+				<h3>Ebay ECE</h3>
+			</div>
+
+			<ul class="list-unstyled components">
+				<p>Sections</p>
+				<li class="active">
+					<!-- data toggle = collaps c'est pour drop down menu et la class pour ajouter le petit triangle a cote -->
+					<a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Catégories</a>
+					<ul class="collapse list-unstyled" id="homeSubmenu">
+						<li>
+							<a href="PageFerrailleOuTresor.php">Ferrailes ou Trésor</a>
+						</li>
+						<li>
+							<a href="PageBonPourMusee.php">Bon pour le musée</a>
+						</li>
+						<li>
+							<a href="PageVIP.php">Accesoire VIP</a>
+						</li>
+
+					</ul>
+				</li>
+
+				<li>
+					<!-- aria expanded pour definir l'etat du menu deroullant false = derme par exemple -->
+					<a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Achat</a>
+					<ul class="collapse list-unstyled" id="pageSubmenu">
+						<li>
+							<a href="PageEnchere.php">Enchéresr</a>
+						</li>
+						<li>
+							<a href="PageAcheterMaintenant.php">Achetez le maintenant</a>
+						</li>
+						<li>
+							<a href="PageMeilleureOffre.php">Meilleure offre</a>
+						</li>
+					</ul>
+				</li>
+
+				<li>
+					<a href="#">Vendre</a>
+				</li>
+
+				<li>
+					<a href="#" class = "icon-user"> VotreCompte</a>
+				</li>
+
+				<li>
+					<a href="#" class = "icon-cart"> Panier</a>
+				</li>
+
+				<li>
+					<a href="AdminLogin.php" class = "icon-user"> Admin</a>
+				</li>
+        
+    </ul>
+
+    <ul class="list-unstyled CTAs">
+    	
+    	<li>
+    		<a href="PageAccueil.php" class="backhome">EbayECE : Home</a>
+    	</li>
+    </ul>
+
+    <ul>
+    	<p>CONTACT<br>
+    		37, quai de Grenelle, 75015 Paris, France <br>
+					info@ebay.ece.fr <br>
+					+33 01 02 03 04 05 <br>
+					+33 01 03 02 05 04
+    	</p>
+    </ul>
+
+</nav>
+
+
+
+
+<!-- Page Content -->
+<div id="content">
+	<!-- LA BARRE BLANCHE DU HAUT -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+
+			<button type="button" id="sidebarCollapse" class="btn btn-info">
+				<i class="fas fa-align-left"></i>
+				<span>BIENVENUE CHEZ EBAY ECE</span>
+			</button>
+
+			<button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<i class="fas fa-align-justify"></i>
+			</button>
+
+		</div>
+	</nav>
+
+
+	<div class="container-fluid">
+
+          <div class="rowA">
+
+            <div class="col-lg-6">
+
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Bonjour <?php echo $_SESSION['PrenomAcheteur']; ?> </h6>
+                </div>
+                <div class="card-body">
+                	<p>Faites des offres sur ID : <?php echo $_POST['IDItem']; ?></p>
+
+                	<header>
+                		<h1>Chat avec le vendeur de ce produit !</h1>
+                	</header>
+
+                	<section class="chat">
+                		<div class="messages">
+
+                		</div>
+                		<div class="user-inputs">
+                			<form action="ChatOffrePHP.php?task=write" method="POST">
+                				<input type="null" name="author" id="author"class = "form-control" value="<?php echo $_SESSION['PrenomAcheteur']; ?> : <?php echo $_POST['IDItem']; ?>" readonly >
+                				<input type="number" id="contentt" class="form-control" name="contentt" placeholder="Votre offre en € !">
+                				<br>
+
+                				<?php 
+                				
+                				for($compt = 1; $compt<6; $compt++)
+                				{
+                					?>
+                					
+                					<button type="submit" class="btn btn-primary" onClick = "this.style.visibility= 'hidden';"> 💬 Send Offre : <?php echo $compt; ?></button>
+                					
+
+                					<?php
+                					
+                				} 
+                				?>
+
+                				<?php 
+
+                					$_SESSION['IDItem'] = $_POST['IDItem'];
+
+                				?>
+                				
+                			</form>
+                		</div>
+                		
+                	</section>
+
+                	<script type="text/javascript">
+											
+						author = "<?php echo $_SESSION['PrenomAcheteur']; ?>  <?php echo $_POST['IDItem']; ?>";
+
+						function getMessages(){
+						  
+						  const requeteAjax = new XMLHttpRequest();
+						  requeteAjax.open("GET", "ChatOffrePHP.php");
+
+						  
+						  requeteAjax.onload = function(){
+						    const resultat = JSON.parse(requeteAjax.responseText);
+						    const html = resultat.reverse().map(function(message){
+						      return `
+						        <div class="message">
+						          <span class="date">${message.created_at.substring(11, 16)}</span>
+						          <span class="author">${message.author}</span> : 
+						          <span class="contentt">${message.contentt}</span>
+						        </div>
+						      `
+						    }).join('');
+
+						    const messages = document.querySelector('.messages');
+
+						    messages.innerHTML = html;
+						    messages.scrollTop = messages.scrollHeight;
+						  }
+
+						  
+						  requeteAjax.send();
+						}
+
+						
+
+						function postMessage(event){
+						  
+						  event.preventDefault();
+
+						  const author = document.querySelector('#author');
+						  const contentt = document.querySelector('#contentt');
+
+						  const data = new FormData();
+						  data.append('author', author.value);
+						  data.append('contentt', contentt.value);
+
+						  const requeteAjax = new XMLHttpRequest();
+						  requeteAjax.open('POST', 'ChatOffrePHP.php?task=write');
+						  
+						  requeteAjax.onload = function(){
+						    contentt.value = '';
+						    contentt.focus();
+						    getMessages();
+						  }
+
+						  requeteAjax.send(data);
+						}
+
+						document.querySelector('form').addEventListener('submit', postMessage);
+
+						
+						const interval = window.setInterval(getMessages, 3000);
+						
+						getMessages();
+                	</script>
+
+
+                </div>
+              </div>
+
+              
+
+            </div>
+    </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="container-fluid">
+
+          <div class="rowA">
+
+            <div class="col-lg-6">
+
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary"> <?php echo $_SESSION['PrenomAcheteur']; ?> Voici La Réponse Finale De Votre Offre</h6>
+                </div>
+                <div class="card-body">
+                	<p>RELOAD VOTRE PAGE POUR OBTENIR LA REPONSE FINALE, MERCI</p>
+                	
+                	<?php
+
+						try {
+							$bdd = new PDO('mysql:host=localhost;dbname=chat;charset=utf8', 'root', 'root');
+						}
+						catch (Exception $e){
+							die('Erreur : ' . $e->getMessage());
+						}
+
+
+						$requete = $bdd->prepare('SELECT * FROM messages WHERE contentt LIKE "OUI'.$_SESSION['PrenomAcheteur'].''.$_POST['IDItem'].'" ');
+
+						$requete->execute();
+	
+						// while ($data = $requete->fetch()) {
+
+						// 	echo '<li> Le produit ID : '.$_POST['IDItem'].'  vous appartient. Transaction : '.$data['id'].'</li>';
+
+						// 	echo "Vous allez recevoir une confirmation trés vite, Merci.";
+
+						// 	}
+
+
+						     
+							// $author = $_SESSION['PrenomAcheteur'].$_SESSION['IDItem'];
+							$data = $requete->fetch();
+								
+							if ($data['contentt'] =="OUI".$_SESSION['PrenomAcheteur'].$_POST['IDItem']) {
+
+								
+								// echo "<script> alert (\"la variable est nulle\")</script";
+								echo '<li> Le produit ID : '.$_POST['IDItem'].'  vous appartient. Transaction : '.$data['id'].'</li>';
+
+						    	echo "Vous allez recevoir une confirmation trés vite, Merci.";
+
+						    	// ENVOYER UN MAIL DE CONFIRMATION D'ACHAT À L'ACHETEUR
+						    	$header="MIME-Version: 1.0\r\n";
+						    	$header.='From:"EbayECE"<projetpiscineweb2020@gmail.com>'."\n";
+						    	$header.='Content-Type:text/html; charset="utf-8"'."\n";
+						    	$header.='Content-Transfer-Encoding: 8bit'; 
+
+						    	$message='
+						    	<html>
+						    	<body>
+						    	<div align="center">
+
+						    	<br />
+						    	J\'ai envoyé ce mail avec PHP !
+						    	<br />
+						    	<a href="http://localhost:8888/ProjetWeb/PageAccueil.php">EbayECE</a>
+
+						    	</div>
+						    	</body>
+						    	</html>
+						    	';
+
+						    	mail($_SESSION['EmailAcheteur'], "Confirmation de votre commande ", $message, $header);
+
+						    	
+
+								try {
+									$bdd = new PDO('mysql:host=localhost;dbname=projetpiscineweb;charset=utf8', 'root', 'root');
+								}
+								catch (Exception $e){
+									die('Erreur : ' . $e->getMessage());
+								}
+
+								$requete1 = $bdd->prepare('DELETE FROM item WHERE IDItem="'.$_POST['IDItem'].'"');
+								$requete1->execute();
+								$requete1->closeCursor();
+								$requete->closeCursor();
+
+
+						}
+
+							?>
+							
+
+							
+							
+
+
+                </div>
+            </div>
+           </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<!-- Footer -->
+      <footer class="sticky-footer ">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>&copy; 2020 Copyright | Droit d'auteur: Tim et Dany</span>
+            <a href="mailto:dany.tadrous.edu.ece.fr">dany.tadrous@edu.ece.fr</a>
+
+          </div>
+        </div>
+      </footer>
+      <!-- FIN DU Footer -->
+
+
+</div>
+
+</div>   
+
+
+
+
+
+
+<!-- jQuery CDN - Slim version (=without AJAX) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- Popper.JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<!-- jQuery Custom Scroller CDN = SLIDER -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(function () {
+		$("#sidebar").mCustomScrollbar({
+			theme: "minimal"
+		});
+
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar, #content').toggleClass('active');
+			$('.collapse.in').toggleClass('in');
+			$('a[aria-expanded=true]').attr('aria-expanded', 'false');
+		});
+	});
+</script>
+
+</body>
+</html>
---
+++ CHAT POUR OFFRE/ChatOffreVendPHP.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+
+$db = new PDO('mysql:host=localhost;dbname=chat;charset=utf8', 'root', 'root', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
+ 
+// CE CHAT M'A ETE PERMIS PAR LE VISIONNAGE DE PLUSIERS TUTO SUR AJAX ET LA CREATION DE CHAT ET LA REALISATION DE TP SUR INTERNET 
+
+// ANALYSE DE LA DEMANDE FAITE VIA URL (GET) POUR DETERMINER SI ON VEUT RECUP OU ECRIRE UN MESSAGE 
+
+$task = "list";
+
+if(array_key_exists("task", $_GET)){
+  $task = $_GET['task'];
+}
+
+if($task == "write"){
+  postMessage();
+} else {
+  getMessages();
+}
+
+// POUR RECUP FAUT ENVOYER DU JSON
+
+function getMessages(){
+  // VAR DISPO A L'EXTERIEURE DE LA FONCTION POUR POUVOIR L'UTILISER À L'INTERIEURE DE LA FONCTION
+  global $db;
+
+  //Requête la base de données pour sortir les 10 derniers messages
+  $resultats = $db->query("SELECT * FROM messages ORDER BY created_at DESC LIMIT 10");
+  //traite les résultats
+  $messages = $resultats->fetchAll();
+  //affiche les données sous forme de JSON
+  echo json_encode($messages);
+}
+
+ // ECRIRE = ANALYSER LES PARAMETRES ENVOYÉS EN POST ET LES SAVE DANS LA BDD
+ 
+function postMessage(){
+  global $db;
+  // 'author' = ""echo $_SESSION['PrenomAcheteur'];""
+  //Analyse des paramètres passés en POST
+  
+  if(!array_key_exists('author', $_POST) || !array_key_exists('contentt', $_POST)){
+
+    echo json_encode(["status" => "error", "message" => "One field or many have not been sent"]);
+    return;
+
+  }
+
+  // $author = $_POST['author'];
+  $author = $_SESSION['NomVendeur'];
+  $contentt = $_POST['contentt'];
+
+
+  //Requête qui permettra d'insérer ces données dans la BDD
+  $query = $db->prepare('INSERT INTO messages SET author = :author, contentt = :contentt, created_at = NOW()');
+  $query->execute([
+    "author" => $author,
+    "contentt" => $contentt
+  ]);
+
+
+  
+
+  //Statut de succes ou d'erreur au format JSON
+  echo json_encode(["status" => "success"]);
+}
+
+
